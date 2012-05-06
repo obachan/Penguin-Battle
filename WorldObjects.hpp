d6fd146665cb4de1038ea1cefb6962cde28d7a5a
Xuejian Yu <tomyu@utexas.edu>
2012-05-06 10:56:11 -0500

    Refactored Terrain into WorldObjectTerrain class

--- WorldObjects.hpp
+++ WorldObjects.hpp
@@ -10,17 +10,18 @@
 #include "WorldObjectPenguin.hpp"
 #include "WorldObjectRoom.hpp"
 #include "WorldObjectPaddle.hpp"
+#include "WorldObjectTerrain.hpp"
 
-class Terrain 
-{
-public:
-
-	Terrain(Ogre::SceneManager*);
-	~Terrain();
-	
-	Ogre::SceneNode* 	terrainNode;
-	Ogre::Entity* 		terrainEntity;
-};
+//====================================================================
+// Yep this class has nothing in it and I know you are 
+// wondering why. This class used to have an insane amount of lines and was
+// extremely bloated. I refactored the big ass code into smaller
+// classes so this just became a file with only header declarations.
+// That way, I don't have the modify the headers of GameState, ServerState,
+// and ClientState. I think it's fine this way for the scope of this 
+// project. If there is reason to refactor, then we can go that 
+// route also.
+//====================================================================
 
 //|||||||||||||||||||||||||||||||||||||||||||||||
  
