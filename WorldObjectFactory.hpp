2c6d55e64695f581f9a7056ac0f212459d31188e
Xuejian Yu <tomyu@utexas.edu>
2012-05-07 14:54:37 -0500

    Added WorldObjectFactory

---
+++ WorldObjectFactory.hpp
@@ -0,0 +1,24 @@
+//|||||||||||||||||||||||||||||||||||||||||||||||
+ 
+#ifndef WORLD_OBJECT_FACTORY_HPP
+#define WORLD_OBJECT_FACTORY_HPP
+ 
+//|||||||||||||||||||||||||||||||||||||||||||||||
+
+#include "WorldObjectBall.hpp"
+#include "WorldObjectGoal.hpp"
+#include "WorldObjectPenguin.hpp"
+#include "WorldObjectRoom.hpp"
+#include "WorldObjectPaddle.hpp"
+#include "WorldObjectTerrain.hpp"
+
+class WorldObjectFactory {
+public:
+	void createNewBall();
+};
+
+//|||||||||||||||||||||||||||||||||||||||||||||||
+ 
+#endif
+ 
+//|||||||||||||||||||||||||||||||||||||||||||||||
